35d39ce4e929c0925589be728996c0c9463fcec2
KauaVL <kauavaleleao@gmail.com>
2025-01-08 21:50:41 -0300

    Gerenciamento de alunos 1.0

--- functions.py
+++ functions.py
@@ -1,43 +1,88 @@
+class Pessoa:
+    def __init__(self,nome,gmail):
+        self.__nome = nome
+        self.__gmail = gmail
+
+    def get_nome(self):
+        return self.__nome
+        
+    def get_gmail(self):
+        return self.__gmail
+
+
+class Aluno(Pessoa):
+
+    __mat = 250000000
+
+    def __init__(self,nome,gmail):
+        super().__init__(nome,gmail)
+        self.__matricula = Aluno.__mat
+        Aluno.__mat += 1
+    
+    def get_matricula(self):
+        return self.__matricula
+    
+    def exibir(self):  
+        print(f"Aluno: {self.get_nome()} - Matrícula: {self.__matricula}")
+    
+
+class Materia():
+    def __init__(self,nome,codigo):
+        self.__codigo = codigo
+        self.__nome = nome
+    
+    def get_codigo(self):
+        return self.__codigo
+    
+    def get_nome(self):
+        return self.__nome
+    
+    def exibir(self):  
+        print(f"Materia: {self.get_nome()} - Código: {self.__codigo}")
+
 
 def home():
     print("----------Sistema de Gerenciamento------------")
     print("1 - Gerenciar Alunos")
     print("2 - Gerenciar Materias")
-    print("3 - Sair")
+    print("3 - Sair") 
     return input("--> ")
 
-def Materias(nt):
+def Materias():
     print("----------------------")
-    print(nt," Materias encontradas")
     print("1 - Cadastrar Materia")
     print("2 - Excluir Materia")
     print("3 - Listar Materias")
+    print("4 - Exibir Última Materia Cadastrada")
     return input("--> ")
 
-def alunos(na):
+
+def alunos():
     print("----------------------")
-    print(na," Alunos encontrados")
     print("1 - Cadastrar Aluno")
     print("2 - Remover Aluno")
     print("3 - Listar Alunos")
+    print("4 - Exibir Último Aluno Cadastrado")
     return input("--> ")
 
-def cadastroaluno(alunos, cmat):
+
+def cadastroaluno(alunos):
     print("----------------------")
     nome = input("Nome completo do Aluno: ")
     gmail = input("Gmail do Aluno: ")
 
     for aluno in alunos.values():
-        if aluno["nome"] == nome:
+        if aluno.get_nome() == nome:
             print("Erro: Nome já cadastrado")
             print("----------------------")
-            return cmat  
+            return  
 
-    alunos[cmat] = {"nome": nome, "gmail": gmail}
-    print(f"Aluno Cadastrado com Sucesso! Matrícula: {cmat}")
+    novo_aluno = Aluno(nome , gmail)
+    alunos[novo_aluno.get_matricula()] = novo_aluno
+    print(f"Aluno Cadastrado com Sucesso! Matrícula: {novo_aluno.get_matricula()}")
     print("----------------------")
     
-    return cmat + 1
+    return 
 
 def cadastroMateria(Materias):
     print("----------------------")
@@ -45,10 +90,9 @@ def cadastroMateria(Materias):
     CodigoMateria = input("Codigo da Materia: ")
     if CodigoMateria in Materias:
          print("Erro: Codigo de Materia ja cadastrado")
-    elif NomeMat in Materias:
-        print("Erro: Materia ja Cadastrada")
     else:
-        Materias[CodigoMateria] = {"materia": NomeMat}
+        nova_materia = Materia(NomeMat, CodigoMateria)
+        Materias[CodigoMateria] = nova_materia
         print("Materia Cadastrada com sucesso!")
         print("----------------------")
 
@@ -58,7 +102,7 @@ def removerMateria(Materias):
     if x not in Materias:
          print("Erro: Codigo de Materia nao encontrado")
     else:
-        nomer = Materias[x]["materia"]
+        nomer = Materias[x].get_nome()
         del Materias[x]
         print(f"Materia {nomer} removida com sucesso!")
 
@@ -68,7 +112,7 @@ def removerAluno(alunos):
     if int(x) not in alunos:
          print("Erro: Matricula nao encontrada")
     else:
-        nomer = alunos[int(x)]["nome"]
+        nomer = alunos[int(x)].get_nome()
         del alunos[int(x)]
         print(f"Aluno {nomer} removido com sucesso!")
 
@@ -77,8 +121,8 @@ def listarM(dict):
         print("Nenhuma Materia Cadastrada.")
     else:
         n = 0 
-        for codigo,dados in dict.items():
-            print(f"{n}- Codigo:{codigo} Materia:{dados['materia']}  ")
+        for materia in dict.values():
+            print(f"{n} - Codigo:{materia.get_codigo()}   Materia:{materia.get_nome()}  ")
             n += 1
 
 def listarA(dict):
@@ -86,6 +130,13 @@ def listarA(dict):
         print("Nenhuma Aluno Cadastrado.")
     else:
         n = 1
-        for c,dados in dict.items():
-            print(f"{n} - Matricula:{c} Nome:{dados['nome']} Gmail:{dados['gmail']} \n")
+        for dados in dict.values():
+            print(f"{n} - Matricula:{dados.get_matricula()} Nome:{dados.get_nome()} Gmail:{dados.get_gmail()} \n")
             n += 1
+
+def ultimo_cadastrado(colecao):
+    if colecao:
+        ultimo_item = list(colecao.values())[-1] 
+        ultimo_item.exibir()  
+    else:
+        print("Nenhum item cadastrado.")
--- index.py
+++ index.py
@@ -2,27 +2,31 @@ import functions
 
 Alunos = {}
 Materias = {}
-cmat = 250000000
-
 
 while True:
-
     x = functions.home()
+
     if x == "1":
-        x2 = functions.alunos(len(Alunos))
+        x2 = functions.alunos()
         if x2 == "1":
-            cmat = functions.cadastroaluno(Alunos,cmat)
+            functions.cadastroaluno(Alunos)
         elif x2 == "2":
             functions.removerAluno(Alunos)
         elif x2 == "3":
             functions.listarA(Alunos)
+        elif x2 == "4":  # Nova opção para ver o último aluno
+            functions.ultimo_cadastrado(Alunos)
+
     elif x == "2":
-        x2 = functions.Materias(len(Materias))
+        x2 = functions.Materias()
         if x2 == "1":
             functions.cadastroMateria(Materias)
         elif x2 == "2":
             functions.removerMateria(Materias)
         elif x2 == "3":
             functions.listarM(Materias)
+        elif x2 == "4":  # Nova opção para ver a última matéria
+            functions.ultimo_cadastrado(Materias)
+
     elif x == "3":
         break
